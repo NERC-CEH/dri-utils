f7455e305257377a3132fc541c4f75d23616d15d
Samantha Hewkin <samhew@ceh.ac.uk>
2025-12-16 13:06:19 +0000

    Switch to pytest

--- tests/io/test_aws.py
+++ tests/io/test_aws.py
@@ -1,58 +1,60 @@
-import unittest
-from unittest.mock import MagicMock, Mock
-from driutils.io.aws import S3Writer, S3Reader
+from typing import Any
+from unittest.mock import MagicMock
+
 import boto3
 import moto
+import pytest
 from botocore.client import BaseClient
-from mypy_boto3_s3.client import S3Client
-from parameterized import parameterized
 from botocore.exceptions import ClientError
+from mypy_boto3_s3.client import S3Client
+
+from driutils.io.aws import S3Reader, S3Writer
 
 BUCKET_NAME = "test_bucket"
 TEST_KEY = "test_key"
 TEST_OBJECT = b'{ "test": "object" }'
 TEST_TAGS = {"tag1": "test1", "tag2": "test2"}
 
-@moto.mock_aws
-class TestS3Writer(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.s3_client: S3Client = boto3.client("s3") #type: ignore
-        self.s3_client.create_bucket(
+@moto.mock_aws
+class TestS3Writer:
+    def setup_s3_client(self) -> None:
+        s3_client: S3Client = boto3.client("s3")  # type: ignore
+        s3_client.create_bucket(
             Bucket=BUCKET_NAME,
             CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
+        return s3_client
 
-    def test_s3_client_type(self):
+    def test_s3_client_type(self) -> None:
         """Returns an object if s3_client is of type `boto3.client.s3`, otherwise
         raises an error"""
-
+        s3_client = self.setup_s3_client()
         # Happy path
-        writer = S3Writer(self.s3_client)
+        writer = S3Writer(s3_client)
 
-        self.assertIsInstance(writer._connection, BaseClient)
+        assert isinstance(writer._connection, BaseClient)
 
         # Bad path
-        
-        with self.assertRaises(TypeError):
-            S3Writer("not an s3 client") #type: ignore
+        with pytest.raises(TypeError):
+            S3Writer("not an s3 client")  # type: ignore
 
-
-    @parameterized.expand([1, "body", 1.123, {"key": b"bytes"}])
-    def test_error_raises_if_write_without_bytes(self, body):
+    @pytest.mark.parametrize("body", [1, "body", 1.123, {"key": b"bytes"}])
+    def test_error_raises_if_write_without_bytes(self, body: Any) -> None:
         """Tests that a type error is raised if the wrong type body used"""
-
-        writer = S3Writer(self.s3_client)
+        s3_client = self.setup_s3_client()
+        writer = S3Writer(s3_client)
         writer._connection = MagicMock()
-        with self.assertRaises(TypeError):
+
+        with pytest.raises(TypeError):
             writer.write(BUCKET_NAME, TEST_KEY, body)
-        
+
         writer._connection.put_object.assert_not_called()
 
-    def test_write_called(self):
+    def test_write_called(self) -> None:
         """Tests that the writer can be executed"""
-
-        writer = S3Writer(self.s3_client)
+        s3_client = self.setup_s3_client()
+        writer = S3Writer(s3_client)
         writer._connection = MagicMock()
         writer.write(BUCKET_NAME, TEST_KEY, TEST_OBJECT)
 
@@ -60,11 +62,12 @@ class TestS3Writer(unittest.TestCase):
 
     def test_object_written(self) -> None:
         """Test the object is written correctly"""
-        writer = S3Writer(self.s3_client)
+        s3_client = self.setup_s3_client()
+        writer = S3Writer(s3_client)
 
         writer.write(BUCKET_NAME, TEST_KEY, TEST_OBJECT, TEST_TAGS)
-        objectBody = self.s3_client.get_object(Bucket=BUCKET_NAME, Key=TEST_KEY)["Body"].read()
-        object_tags = self.s3_client.get_object_tagging(Bucket=BUCKET_NAME, Key=TEST_KEY)["TagSet"]
+        objectBody = s3_client.get_object(Bucket=BUCKET_NAME, Key=TEST_KEY)["Body"].read()
+        object_tags = s3_client.get_object_tagging(Bucket=BUCKET_NAME, Key=TEST_KEY)["TagSet"]
 
         expected_body = b'{ "test": "object" }'
         expected_tags = [{"Key": "tag1", "Value": "test1"}, {"Key": "tag2", "Value": "test2"}]
@@ -72,36 +75,39 @@ class TestS3Writer(unittest.TestCase):
         assert objectBody == expected_body
         assert object_tags == expected_tags
 
-class TestS3Reader(unittest.TestCase):
+
+class TestS3Reader:
     """Test suite for the S3 client reader"""
 
-    @classmethod
-    def setUpClass(cls) -> None:
+    bucket = "my-bucket"
+    key = "my-key"
 
-        cls.s3_client: S3Client = boto3.client("s3") #type: ignore
-        cls.bucket = "my-bucket"
-        cls.key = "my-key"
+    @classmethod
+    def setup_s3_client(cls) -> None:
+        s3_client: S3Client = boto3.client("s3")  # type: ignore
+        return s3_client
+
     def test_error_caught_if_read_fails(self) -> None:
         """Tests that a ClientError is raised if read fails"""
+        s3_client = self.setup_s3_client()
 
-        reader = S3Reader(self.s3_client)
-        fake_error =  ClientError(operation_name='InvalidKeyPair.Duplicate', error_response={
-            'Error': {
-                'Code': 'Duplicate', 
-                'Message': 'This is a custom message'
-            }
-        })
+        reader = S3Reader(s3_client)
+
+        fake_error = ClientError(
+            operation_name="InvalidKeyPair.Duplicate",
+            error_response={"Error": {"Code": "Duplicate", "Message": "This is a custom message"}},
+        )
         reader._connection.get_object = MagicMock(side_effect=fake_error)
 
-        with self.assertRaises((RuntimeError, ClientError)):
+        with pytest.raises((RuntimeError, ClientError)):
             reader.read(self.bucket, self.key)
 
     def test_get_request_made(self) -> None:
         """Test that the get request is made to s3 client"""
+        s3_client = self.setup_s3_client()
 
-        reader = S3Reader(self.s3_client)
+        reader = S3Reader(s3_client)
         reader._connection = MagicMock()
-        
 
         reader.read(self.bucket, self.key)
 
--- tests/metadata_api/test_metadata_utils.py
+++ tests/metadata_api/test_metadata_utils.py
@@ -1,139 +1,168 @@
 import re
-from unittest import TestCase
+from typing import Any
 
-from parameterized import parameterized
+import pytest
 
-from driutils.metadata_api.utils import URI_ID_EXTRACT_REGEX, SITE_ID_EXTRACT_REGEX, get_property, check_single_list_item
+from driutils.metadata_api.utils import (
+    SITE_ID_EXTRACT_REGEX,
+    URI_ID_EXTRACT_REGEX,
+    check_single_list_item,
+    get_property,
+)
 
 
-class TestUriIdExtractRegex(TestCase):
-    @parameterized.expand(
+class TestUriIdExtractRegex:
+    @pytest.mark.parametrize(
+        "string, expected",
         [
-            ("simple", "http://example.com/parameter/simple", "simple"),
-            ("with_hyphen", "http://example.com/parameter/single-hyphen", "single-hyphen"),
-            ("with_multiple_hyphen", "http://example.com/parameter/multiple-hyphen-test", "multiple-hyphen-test"),
-            ("with_underscore", "http://example.com/parameter/under_score", "under_score"),
-            ("with_multiple_underscore", "http://example.com/parameter/multiple_under_score", "multiple_under_score"),
-            ("hypen_underscore", "http://example.com/parameter/mix-hyphen_underscore", "mix-hyphen_underscore"),
-            ("with_numbers", "http://example.com/parameter/g1", "g1"),
-        ]
+            ("http://example.com/parameter/simple", "simple"),
+            ("http://example.com/parameter/single-hyphen", "single-hyphen"),
+            ("http://example.com/parameter/multiple-hyphen-test", "multiple-hyphen-test"),
+            ("http://example.com/parameter/under_score", "under_score"),
+            ("http://example.com/parameter/multiple_under_score", "multiple_under_score"),
+            ("http://example.com/parameter/mix-hyphen_underscore", "mix-hyphen_underscore"),
+            ("http://example.com/parameter/g1", "g1"),
+        ],
+        ids=[
+            "simple",
+            "with_hyphen",
+            "with_multiple_hyphen",
+            "with_underscore",
+            "with_multiple_underscore",
+            "hypen_underscore",
+            "with_numbers",
+        ],
     )
-    def test_parsing_success(self, _, string, expected):
+    def test_parsing_success(self, string: str, expected: str) -> None:
         """Test that validation passes when string format meets the regex."""
         result = re.match(URI_ID_EXTRACT_REGEX, string).group(1)
-        self.assertEqual(result, expected)
+        assert result == expected
 
-    @parameterized.expand(
-        [
-            ("not_a_url", "invalid-format-not-url"),
-            ("empty_string", ""),
-            ("no_name_after_slash", "http://example.com/parameter/"),
-            ("special_character", "http://example.com/parameter/test&value"),
-        ]
+    @pytest.mark.parametrize(
+        "string",
+        ["invalid-format-not-url", "", "http://example.com/parameter/", "http://example.com/parameter/test&value"],
+        ids=[
+            "not_a_url",
+            "empty_string",
+            "no_name_after_slash",
+            "special_character",
+        ],
     )
-    def test_parsing_none(self, _, string):
+    def test_parsing_none(self, string: str) -> None:
         """Test that no result matched when string format that fails the regex."""
         result = re.match(URI_ID_EXTRACT_REGEX, string)
-        self.assertIsNone(result)
+        assert result is None
 
 
-class TestSiteIdExtractRegex(TestCase):
-    @parameterized.expand(
+class TestSiteIdExtractRegex:
+    @pytest.mark.parametrize(
+        "string, expected",
         [
-            ("all_string", f"http://fdri.ceh.ac.uk/id/site/cosmos-chimn", "chimn"),
-            ("with_numbers", f"http://fdri.ceh.ac.uk/id/site/cosmos-alic1", "alic1"),
-        ]
+            ("http://fdri.ceh.ac.uk/id/site/cosmos-chimn", "chimn"),
+            ("http://fdri.ceh.ac.uk/id/site/cosmos-alic1", "alic1"),
+        ],
+        ids=[
+            "all_string",
+            "with_numbers",
+        ],
     )
-    def test_parsing_success(self, _, string, expected):
+    def test_parsing_success(self, string: str, expected: str) -> None:
         """Test that validation passes when string format meets the regex."""
         result = re.match(SITE_ID_EXTRACT_REGEX, string).group(1)
-        self.assertEqual(result, expected)
+        assert result == expected
 
-    @parameterized.expand(
+    @pytest.mark.parametrize(
+        "string",
         [
-            ("not_a_url", "invalid-format-not-url"),
-            ("empty_string", ""),
-            ("no_name_after_slash", "http://example.com/id/site/"),
-            ("special_character", "http://example.com/id/site/cosmos-test&site"),
-            ("multiple_hyphens", "http://example.com/id/site/cosmos-test-site-id"),
-            ("only_numbers", "http://example.com/id/site/12345"),
-        ]
+            "invalid-format-not-url",
+            "",
+            "http://example.com/id/site/",
+            "http://example.com/id/site/cosmos-test&site",
+            "http://example.com/id/site/cosmos-test-site-id",
+            "http://example.com/id/site/12345",
+        ],
+        ids=[
+            "not_a_url",
+            "empty_string",
+            "no_name_after_slash",
+            "special_character",
+            "multiple_hyphens",
+            "only_numbers",
+        ],
     )
-    def test_parsing_none(self, _, string):
+    def test_parsing_none(self, string: str) -> None:
         """Test that no result matched when string format that fails the regex."""
         result = re.match(SITE_ID_EXTRACT_REGEX, string)
-        self.assertIsNone(result)
+        assert result is None
 
-class TestGetProperty(TestCase):
+
+class TestGetProperty:
     """Test the get_property function."""
 
-    def test_get_property_int_float_str(self) -> None:
-        """Test extracting an int property"""
+    @pytest.mark.parametrize(
+        "value, expected",
+        [(123, 123), (123.4, 123.4), ("123", "123"), (["123"], "123"), (None, None), ("", ""), (0, 0)],
+        ids=["int", "float", "string", "string list", "None", "empty string", "zero"],
+    )
+    def test_get_property(self, value: Any, expected: Any) -> None:
+        """Test extracting a property"""
         key = "abc"
 
-        expected = 123
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
+        result = get_property(key, {key: value})
+        assert result == expected
 
-        expected = 123.4
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
-
-        expected = "123"
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
-
-    def test_get_property_list(self) -> None:
-        """Test extracting a string property"""
-        key = "abc"
-        expected = "123"
-        result = get_property(key, {key: [expected]})
-        self.assertEqual(result, expected)
+    # def test_get_property_list(self) -> None:
+    #     """Test extracting a string property"""
+    #     key = "abc"
+    #     expected = "123"
+    #     result = get_property(key, {key: [expected]})
+    #     assert result == expected
 
     def test_get_property_none(self) -> None:
         """Test trying to extract a None dictionary"""
         key = "abc"
         expected = None
         result = get_property(key, None)
-        self.assertEqual(result, expected)
+        assert result == expected
 
-    def test_get_property_falsy_value(self) -> None:
-        """Test extracting a falsy value"""
-        key = "abc"
+    # def test_get_property_falsy_value(self) -> None:
+    #     """Test extracting a falsy value"""
+    #     key = "abc"
 
-        expected = None
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
+    #     expected = None
+    #     result = get_property(key, {key: expected})
+    #     assert result == expected
 
-        expected = ""
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
+    #     expected = ""
+    #     result = get_property(key, {key: expected})
+    #     assert result == expected
 
-        expected = 0
-        result = get_property(key, {key: expected})
-        self.assertEqual(result, expected)
+    #     expected = 0
+    #     result = get_property(key, {key: expected})
+    #     assert result == expected
 
-class TestCheckSingleListItem(TestCase):
+
+class TestCheckSingleListItem:
     """Test the check_single_list_item method"""
 
-    def test_success(self):
+    def test_success(self) -> None:
         """Test method returns the only item from a list"""
-        input = ["test"]
+        input_data = ["test"]
         expected = "test"
 
-        result = check_single_list_item(input)
+        result = check_single_list_item(input_data)
 
-        self.assertEqual(result, expected)
-    
+        assert result == expected
 
-
-    @parameterized.expand(
-        [
-            ("value_error", ["test1", "test2"], ValueError),
-            ("type_error", {"test"}, TypeError)
-        ]
+    @pytest.mark.parametrize(
+        "input_data,error",
+        [(["test1", "test2"], ValueError), ({"test"}, TypeError)],
+        ids=[
+            "value_error",
+            "type_error",
+        ],
     )
-    def test_error(self, _, input, error):
+    def test_error(self, input_data: list | dict, error: Exception) -> None:
         """Test correct errors are raised."""
-        with self.assertRaises(error):
-            check_single_list_item(input)
+        with pytest.raises(error):
+            check_single_list_item(input_data)
