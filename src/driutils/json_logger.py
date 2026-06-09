b370f89e8cc860e1a461d74c2131e7137670dc7f
natsha <natsha@ceh.ac.uk>
2026-06-09 14:18:15 +0100

    add core json log object

--- src/driutils/json_logger.py
+++ src/driutils/json_logger.py
@@ -1,11 +1,23 @@
 import json
-import os
 import traceback
 
-SERVICE_NAME = os.environ.get("SERVICE_NAME", "unknown-service")
 
+def json_formatter(record: dict, service_name: str) -> str:
+    """The core JSON logger object.
+    
+    This object is used throughout our services to create consistent, and
+    machine readable logs.
 
-def json_formatter(record) -> str:
+    Any logs that are specific to a service are within that service deployment
+    and should be added to the 'extras' attribute with the service name key.
+
+    Args:
+        record: the loguru record object
+        service: the service the logs are running for
+    
+    Returns:
+        The log attributes
+    """
     # Handle error messages
     error_type = None
     if record["exception"] is not None:
@@ -16,17 +28,18 @@ def json_formatter(record) -> str:
             "stacktrace": "".join(traceback.format_tb(exc_tb)).strip() if exc_tb else None,
         }
     
-    # placeholder extras for service specifc logs
-
+    # Create core log structure
     log_entry = {
-        "time": record["time"].isoformat(),
+        "time": record["time"].replace(tzinfo=None).isoformat(),
         "msg": record["message"],
         "level": record["level"].name,
-        "service": SERVICE_NAME,
+        "service": service_name,
         "loc": f"{record['name']}:{record['line']}",
         "error_type": error_type,
         "thread": record['thread'].id,
         "extras": dict(record["extra"]) or None,
     }
 
+    # Add new line for each record, and remove single curly brackets ready
+    # for the loguru format callable
     return (json.dumps(log_entry, default=str) + "\n").replace("{", "{{").replace("}", "}}")
